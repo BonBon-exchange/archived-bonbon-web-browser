30b724d9617df744ef09517fd55ba389308611d2
Daniel Febrero <daniel.febrero@belgiantrain.be>
2022-06-10 12:27:28 +0200

    Persist memory after closing app

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable import/prefer-default-export */
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { Addaps } from './components/Addaps';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 export function App() {
   return (
     <Provider store={store}>
-      <Addaps />
+      <PersistGate loading={null} persistor={persistor}>
+        <Addaps />
+      </PersistGate>
     </Provider>
   );
 }
--- src/renderer/store/store.ts
+++ src/renderer/store/store.ts
@@ -1,13 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import addapsReducer from './reducers/Addaps';
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persisteAddaps = persistReducer(persistConfig, addapsReducer);
+
 export const store = configureStore({
   reducer: {
-    addaps: addapsReducer,
+    addaps: persisteAddaps,
   },
   devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
