f48da7f6a2990a5f33a65a03f903c52555976417
Daniel Febrero <daniel.febrero@belgiantrain.be>
2022-06-14 10:58:55 +0200

    Update logo icon

--- src/main/main.ts
+++ src/main/main.ts
@@ -78,7 +78,7 @@ const createWindow = async () => {
     show: false,
     width: 1024,
     height: 728,
-    icon: getAssetPath('icon.png'),
+    icon: getAssetPath('b.png'),
     webPreferences: {
       webviewTag: true,
       preload: app.isPackaged
