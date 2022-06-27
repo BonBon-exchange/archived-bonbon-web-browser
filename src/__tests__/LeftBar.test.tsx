1e8c1c21a96ff5bf080a2014222b16f0e5a178c1
Daniel Febrero <daniel.febrero@belgiantrain.be>
2022-06-27 16:54:12 +0200

    Update LeftBar.test.tsx

--- src/__tests__/LeftBar.test.tsx
+++ src/__tests__/LeftBar.test.tsx
@@ -2,21 +2,29 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import { Middleware } from '@reduxjs/toolkit';
 
 import { LeftBar } from '../renderer/App/components/LeftBar';
 import { mockWindow } from './beforeAll';
-import { initialState } from '../renderer/App/store/reducers/Board';
+import {
+  addBrowser,
+  removeBrowser,
+} from '../renderer/App/store/reducers/Board';
+import { store } from '../renderer/App/store/store';
 
-let store: any;
-const middlewares: Middleware[] = [];
+const addBrowserAction = {
+  id: 'randomid',
+  url: 'https://www.github.com',
+  top: 0,
+  left: 0,
+  height: 800,
+  width: 600,
+  firstRendering: true,
+  favicon: '',
+};
 
 describe('LeftBar', () => {
   beforeAll(() => {
     mockWindow();
-    const mockStore = configureStore(middlewares);
-    store = mockStore({ board: initialState });
   });
 
   it('should render', () => {
@@ -27,5 +35,51 @@ describe('LeftBar', () => {
         </Provider>
       )
     ).toBeTruthy();
+  });
+
+  it('should have one browser because of initial state', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <LeftBar />
+      </Provider>
+    );
+
+    expect(container.getElementsByClassName('LeftBar__browserFav').length).toBe(
+      1
+    );
+  });
+
+  it('should have two browsers in the store', () => {
+    store.dispatch(addBrowser(addBrowserAction));
+    expect(store.getState().board.board.browsers.length).toBe(2);
+  });
+
+  it('should have two browsers in dom', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <LeftBar />
+      </Provider>
+    );
+
+    expect(container.getElementsByClassName('LeftBar__browserFav').length).toBe(
+      2
+    );
+  });
+
+  it('should have one browser in the store', () => {
+    store.dispatch(removeBrowser(addBrowserAction.id));
+    expect(store.getState().board.board.browsers.length).toBe(1);
+  });
+
+  it('should have 1 browser in dom', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <LeftBar />
+      </Provider>
+    );
+
+    expect(container.getElementsByClassName('LeftBar__browserFav').length).toBe(
+      1
+    );
   });
 });
