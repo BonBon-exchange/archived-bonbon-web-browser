98f37ae6872283fbccfe0426ebf86a7d6289ca44
Daniel Febrero <daniel.febrero@belgiantrain.be>
2022-06-27 20:56:19 +0200

    Remove useless classname

--- src/renderer/App/components/ButtonAddBrowser/ButtonAddBrowser.tsx
+++ src/renderer/App/components/ButtonAddBrowser/ButtonAddBrowser.tsx
@@ -11,11 +11,5 @@ import './style.css';
 export const ButtonAddBrowser: React.FC<ButtonAddBrowserProps> = ({
   onClick,
 }) => {
-  return (
-    <AddIcon
-      className="ButtonAddBrowser"
-      id="ButtonAddBrowser"
-      onClick={() => onClick({})}
-    />
-  );
+  return <AddIcon id="ButtonAddBrowser" onClick={() => onClick({})} />;
 };
