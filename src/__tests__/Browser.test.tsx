bfde81cb69a4a1ae1ae3fc23f7cc8c94596f72df
Daniel Febrero <febrero@me.com>
2022-06-27 21:17:36 +0200

    Feature/add tests (#15)
    * Add Addaps.test.tsx
    
    * Add Board.test.tsx
    
    * Add Browser.test.tsx
    
    * Add BrowserControlBar.test.tsx
    
    * Add BrowserTopBar.test.tsx
    
    * Add ButtonAddBrowser.test.tsx
    
    * Add LeftBar.test.tsx
    
    * Add Library.test.tsx
    
    * Add Popup.test.tsx
    
    * Add Settings.test.tsx
    
    * Disable prefer default export
    
    * npm install
    
    * Update LeftBar.test.tsx
    
    * Update Board.test.tsx
    
    * Update Browser.test.tsx
    
    * Remove useless classname
    
    Co-authored-by: Daniel Febrero <daniel.febrero@belgiantrain.be>

---
+++ src/__tests__/Browser.test.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+
+import { Browser } from '../renderer/App/components/Browser';
+import { mockWindow } from './beforeAll';
+import { store } from '../renderer/App/store/store';
+import { toggleBoardFullSize } from '../renderer/App/store/reducers/Board';
+
+const browserProps = {
+  id: '123qsdf',
+  url: 'https://www.github.com',
+  top: 0,
+  left: 0,
+  height: 600,
+  width: 800,
+  firstRendering: true,
+};
+
+describe('Browser', () => {
+  beforeAll(() => {
+    mockWindow();
+  });
+
+  it('should render', () => {
+    expect(
+      render(
+        <Provider store={store}>
+          <Browser {...browserProps} />
+        </Provider>
+      )
+    ).toBeTruthy();
+  });
+
+  it('should be fullsize', () => {
+    store.dispatch(toggleBoardFullSize());
+    const { container } = render(
+      <Provider store={store}>
+        <Browser {...browserProps} />
+      </Provider>
+    );
+
+    const browser = container.getElementsByClassName('Browser__is-full-size');
+    expect(browser.length).toBe(1);
+  });
+});
